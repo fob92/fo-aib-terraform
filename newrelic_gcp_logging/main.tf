e5765c6a784b0e3d0201e3b0349fbd321f8acace
fob92 <felix.oberdorf@gmail.com>
2025-08-16 10:52:05 +0200

    save main

--- newrelic_gcp_logging/main.tf
+++ newrelic_gcp_logging/main.tf
@@ -1,11 +1,15 @@
+locals {
+  sa_newrelic_gcp_logging = "wnr00v58or2@newrelic-gcp.iam.gserviceaccount.com"
+}
+
 resource "google_project_iam_member" "newrelic_viewer" {
 	project = var.project_id
 	role    = "roles/viewer"
-	member  = "serviceAccount:wnr00v58or2@newrelic-gcp.iam.gserviceaccount.com"
+	member  = "serviceAccount:${local.sa_newrelic_gcp_logging}"
 }
 
 resource "google_project_iam_member" "newrelic_service_usage_consumer" {
 	project = var.project_id
 	role    = "roles/serviceusage.serviceUsageConsumer"
-	member  = "serviceAccount:wnr00v58or2@newrelic-gcp.iam.gserviceaccount.com"
+	member  = "serviceAccount:${local.sa_newrelic_gcp_logging}"
 }
